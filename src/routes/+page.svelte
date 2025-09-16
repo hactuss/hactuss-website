bac4faeea8df1576ff07fcf6a4d317b898f91eb4
hactuss <hactuss@proton.me>
2025-09-16 21:21:14 +0200

    new file:   deno.lock 	modified:   src/routes/+page.svelte 	new file:   src/routes/test/+page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,22 @@
+<script>
+    import { browser } from "$app/environment";
+    import { flip } from "svelte/animate";
+    let top_bottom_text = "none";
+    let side_text = "block";
+
+    if (browser) {
+        console.log("width:" + screen.width);
+        if (screen.width < 500) {
+            side_text = "none";
+        }
+    }
+</script>
+
 <main id="main-container" class="flex justify-center content-center">
-    <div class="bg-black w-[100%] h-[100%] flex-1/8">
+    <div
+        style="display: {side_text};"
+        class="bg-black w-[100%] h-[100%] flex-1/8"
+    >
         <h1
             style="font-size: 9rem; writing-mode: vertical-lr; align-bottom"
             class="text-white text-center"
@@ -7,12 +24,19 @@
             Hactus
         </h1>
     </div>
+
     <div class="w-[100%]">
+        <div
+            id="top_bottom_text"
+            style="display: {top_bottom_text}; color: white; background-color: black;"
+        >
+            <div><h1>HACTUS</h1></div>
+        </div>
         <div
             class="flex flex-col justify-center items-center"
             style="justify-self: center;"
         ></div>
-        <div id="window" class="m-[10px]">
+        <div id="window">
             <div id="bar"><p>+hactuss.svelte</p></div>
             <div id="contents">
                 <a href="https://office-haj.vercel.app">go to the office</a>
@@ -28,6 +52,13 @@
                 >
             </div>
         </div>
+
+        <div
+            id="top_bottom_text"
+            style="display: {top_bottom_text}; color: white; background-color: black; position: relative; bottom: 0;"
+        >
+            <div><h1>HACTUS</h1></div>
+        </div>
     </div>
 </main>
 
@@ -40,6 +71,7 @@
 
     #window {
         border: solid 2px black;
+        margin: 10px;
     }
     #bar {
         background-color: black;
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,15 @@
+<!--
+
+get width of element
+
+x= - width of element
+then destroyed
+
+born at max x screen coord
+moved to min x screen coord
+
+
+
+
+
+-->
